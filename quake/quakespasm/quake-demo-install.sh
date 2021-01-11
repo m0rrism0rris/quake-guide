0b7b4e9c13b390e1d630035f570d9f02d31c871b
m0rrism0rris <75397647+m0rrism0rris@users.noreply.github.com>
2021-01-11 12:22:52 -0500

    Update quake-demo-install.sh

--- quake/quakespasm/quake-demo-install.sh
+++ quake/quakespasm/quake-demo-install.sh
@@ -3,18 +3,25 @@ OS="`uname`"
 case $OS in
     'Darwin' )
         cd /Applications
-        if [ -e Quakespasm ]; then echo "Error: Quakespasm is already installed"; exit; fi
+        if [ -e Quakespasm ]; then echo 'Quakespasm appears to be already installed.
+
+To install the full version of Quake, not just the demo, follow the instructions at
+[https://github.com/m0rrism0rris/misc/blob/main/quake/pak1_macosx.md]
+Ignore PAK0.PAK, that comes with the demo'; exit; fi
         mkdir -p Quakespasm; cd Quakespasm
         curl -o QuakeSpasm-0.93.2-osx-SDL2.zip https://ilotfiber.dl.sourceforge.net/project/quakespasm/Mac/QuakeSpasm-0.93.2-sx-SDL2.zip
         unzip QuakeSpasm-0.93.2-osx-SDL2.zip -x quakespasm.pak
         rm QuakeSpasm-0.93.2-osx-SDL2.zip
         ;;
+    'Linux' )
+        echo "Idk how to do this on Linux but if you use it, you're probably much better at this sort of thing than I am. Sorry."
+        exit
 esac
 
 mkdir -p id1; cd id1
 curl -o pak0.pak https://raw.githubusercontent.com/shaunlebron/blinky/master/game/id1/pak0.pak
 curl -o quake_campaign_soundtrack.zip https://www.quaddicted.com/files/music/quake_campaign_soundtrack.zip
-unzip quake_campaign_soundtrack.zip -x quake_campaign_soundtrack/id1/music/track02.ogg quake_campaign_soundtrack/id1/music/track03.ogg quake_campaign_soundtrack/id1/music/track04.ogg quake_campaign_soundtrack/id1/music/track05.ogg quake_campaign_soundtrack/id1/music/track06.ogg quake_campaign_soundtrack/id1/music/track07.ogg quake_campaign_soundtrack/id1/music/track08.ogg quake_campaign_soundtrack/id1/music/track09.ogg quake_campaign_soundtrack/id1/music/track10.ogg quake_campaign_soundtrack/id1/music/track11.ogg quake_campaign_soundtrack/id1/tracklist.cfg quake_campaign_soundtrack/quake_campaign_soundtrack_readme.txt
+unzip quake_campaign_soundtrack.zip -x quake_campaign_soundtrack/id1/music/track02.mp3 quake_campaign_soundtrack/id1/music/track03.mp3 quake_campaign_soundtrack/id1/music/track04.mp3 quake_campaign_soundtrack/id1/music/track05.mp3 quake_campaign_soundtrack/id1/music/track06.mp3 quake_campaign_soundtrack/id1/music/track07.mp3 quake_campaign_soundtrack/id1/music/track08.mp3 quake_campaign_soundtrack/id1/music/track09.mp3 quake_campaign_soundtrack/id1/music/track10.mp3 quake_campaign_soundtrack/id1/music/track11.mp3 quake_campaign_soundtrack/id1/tracklist.cfg quake_campaign_soundtrack/quake_campaign_soundtrack_readme.txt
 mv quake_campaign_soundtrack/id1/music music
 rm -r quake_campaign_soundtrack
 rm quake_campaign_soundtrack.zip
